0059534cc05f9ef2d5b71529290e377f6842238a
aisha0926 <aisha.0926@hotmail.com>
2018-08-15 23:11:57 +0100

    Adding footer - Jevale

---
+++ Desktop/QA-Cinema/QA/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Footer1></Footer1>
+  </div>
+</template>
+
+<script>
+import Footer1 from './components/Footer1'
+
+
+export default {
+  name: 'App',
+  components: {
+
+    Footer1
+
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
